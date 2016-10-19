4ea1ec0acf74ce193568605b87fca4bbb46e3e2d
Ozee31 <flavien.beninca@gmail.com>
2016-10-19 12:58:23 +0000

    fix app.php

--- config/app.php
+++ config/app.php
@@ -346,7 +346,7 @@ return [
 
     'Webpack' => [
       'assets' => [
-        'dev' => 'http://localhost:3003/cakephp-starter/app.js',
+        'dev' => 'http://localhost:3003/app.js',
         'config' => 'assets.json',
       ],
     ],
